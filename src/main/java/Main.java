8cf0f0ac164cb7c3e014929b33953b6a31d36cf0
mksik98 <mksik98@gmail.com>
2023-07-05 22:18:41 +0300

    Не до конца сделано, но не понимаю как сделать

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        People addPeople = new People();
+        Product allProduct = new Product();
+        Calculator addCalculator = new Calculator();
+        addPeople.person();
+        addPeople.morePerson();
+        allProduct.addProduct();
+        allProduct.addMoreProduct();
+        addCalculator.endCalculator(allProduct.fullPrice, addPeople.personCheck);
+
+       // String rub = " Рублей";
+        //float priceForOne;
+       // priceForOne = allProduct.fullPrice / addPeople.personCheck;
+       // if (Math.floor(priceForOne) < 5){
+      //      rub = "Рубля";
+      //  }
+      //  else if (Math.floor(priceForOne) == 1){
+      //      rub = "Рубль";
+      //  }
+       // String.format("%.2f", priceForOne);
+       // System.out.println("Сумма на человека: " + priceForOne + rub);
     }
 }
